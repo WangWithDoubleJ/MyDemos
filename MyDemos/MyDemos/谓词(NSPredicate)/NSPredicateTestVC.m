8049025bbbc4d7fc4667fa50b283bf4e400c47eb
LW <wangjianjun0730@126.com>
2017-07-02 11:32:17 +0800

    谓语测试

--- MyDemos/MyDemos/谓词(NSPredicate)/NSPredicateTestVC.m
+++ MyDemos/MyDemos/谓词(NSPredicate)/NSPredicateTestVC.m
@@ -17,8 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
-    
+    NSArray *arr0 = @[@1,@2,@3,@4];
+    NSArray *arr1 = [NSArray arrayWithObjects:@4,@5, nil];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF in %@",arr0];
+    NSString *arrR1 = [arr1 filteredArrayUsingPredicate:predicate];
     
     
     
